2b663f45bbe268cb4e83bd38dc71cf9ad5b7da67
yxkong <5ycode@sina.com>
2021-04-21 12:15:00 +0800

    代码优化

--- bootstrap-load-client/src/main/java/com/yxkong/agent/data/ThreadPoolMonitorData.java
+++ bootstrap-load-client/src/main/java/com/yxkong/agent/data/ThreadPoolMonitorData.java
@@ -1,7 +1,11 @@
 package com.yxkong.agent.data;
 
+import com.yxkong.agent.NamedThreadFactory;
+import javafx.util.Pair;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 
 /**
@@ -18,24 +22,48 @@ public class ThreadPoolMonitorData {
     }
     /**
      * 对外暴露添加接口
-     * @param name 线程池名称
-     * @param desc 线程池描述
-     * @param threadPoolExecutor
+     * @param executor
      */
-    public static void add(String name,String desc,ThreadPoolExecutor threadPoolExecutor){
-        ThreadPoolExecutorWraper data = new ThreadPoolExecutorWraper(name,desc,threadPoolExecutor);
-        if(!threadPoolExecutorWraperMap.containsKey(name)){
-            threadPoolExecutorWraperMap.put(name,data);
+    public  void add(ThreadPoolExecutor executor){
+        final Pair<String, String> info = getInfo(executor);
+        ThreadPoolExecutorWraper data = new ThreadPoolExecutorWraper(info.getKey(),info.getValue(),executor);
+        put(info.getKey(),data);
+    }
+    private static Pair<String,String> getInfo(ThreadPoolExecutor executor){
+        final ThreadFactory threadFactory = executor.getThreadFactory();
+        String key = executor.getClass().getName()+"@"+executor.hashCode();
+        String desc = "未使用提供的NamedThreadFactory";
+        if(threadFactory instanceof NamedThreadFactory){
+            NamedThreadFactory namedThreadFactory = (NamedThreadFactory) threadFactory;
+            key = namedThreadFactory.getName();
+            desc = namedThreadFactory.getDesc();
+        }
+        return new Pair<>(key,desc);
+    }
+
+    /**
+     * 按照key 加锁
+     * 按实际情况，线程池是稀缺资源，一个项目里不会有特别多
+     * 只有放入了字符串常量池，字符串的对象才会唯一
+     * @param name 线程池的名字
+     * @param wraper  线程池包装类
+     */
+    private void put(String name,ThreadPoolExecutorWraper wraper){
+        synchronized (name.intern()){
+            if(!threadPoolExecutorWraperMap.containsKey(name)){
+                threadPoolExecutorWraperMap.put(name,wraper);
+            }
         }
     }
 
     /**
      * 对外暴露remove接口
-     * @param name
+     * @param executor
      */
-    public static void remove(String name){
-        if(threadPoolExecutorWraperMap.containsKey(name)){
-            threadPoolExecutorWraperMap.remove(name);
+    public static void remove(ThreadPoolExecutor executor){
+        final Pair<String, String> info = getInfo(executor);
+        if(threadPoolExecutorWraperMap.containsKey(info.getKey())){
+            threadPoolExecutorWraperMap.remove(info.getKey());
         }
     }
 }
